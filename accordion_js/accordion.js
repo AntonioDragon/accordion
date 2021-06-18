2e06b760624868df4707766775d3d4bc4e1a8c2c
AntonioDragon <antonio300dragon@gmail.com>
2021-06-18 15:24:56 +0300

    fixed js and add transition

--- accordion_js/accordion.js
+++ accordion_js/accordion.js
@@ -1,38 +1,45 @@
 var check = document.querySelector('.check-block__input');
-var accordion_icon = '.triger__icon';
-var accordion_active = 'active__item';
+var accordion_icon_all = document.querySelectorAll('.trigger__icon');
+var accordion_active = 'active--item';
 
-var src_open ='./icons/open.svg';
-var src_close ='./icons/close.svg';
+var src_open ='icon--open';
+var src_close ='icon--close';
 
-document.querySelectorAll('.item__triger').forEach(item =>
+document.querySelectorAll('.item__trigger').forEach(item =>
     item.addEventListener('click', ()=>{
         var parent = item.parentNode;
-        
+        var accordion_icon = parent.querySelector('.trigger__icon');
+
         if(check.checked){
             parent.classList.toggle(accordion_active);
             if(parent.classList.contains(accordion_active)){
-                parent.querySelector(accordion_icon).src=src_open;
+                accordion_icon.classList.remove(src_close);
+                accordion_icon.classList.add(src_open);
             }
             else{
-                parent.querySelector(accordion_icon).src=src_close;
+                accordion_icon.classList.remove(src_open);
+                accordion_icon.classList.add(src_close);
             }
         }
         else{
             if(parent.classList.contains(accordion_active)){
                 parent.classList.remove(accordion_active);
-                parent.querySelector(accordion_icon).src=src_close;
+                accordion_icon.classList.remove(src_open);
+                accordion_icon.classList.add(src_close);
             }
             else{
                 document.querySelectorAll('.item').forEach((child)=> child.classList.remove(accordion_active));
-                document.querySelectorAll(accordion_icon).forEach((child)=> child.src=src_close);
+                accordion_icon_all.forEach((child)=> child.classList.remove(src_open));
+                accordion_icon_all.forEach((child)=> child.classList.add(src_close));
                 parent.classList.add(accordion_active);
-                parent.querySelector(accordion_icon).src=src_open;
+                accordion_icon.classList.remove(src_close);
+                accordion_icon.classList.add(src_open);
             }
         }
     })   
 )
 check.addEventListener('click',()=>{
     document.querySelectorAll('.item').forEach((child)=> child.classList.remove(accordion_active));
-    document.querySelectorAll(accordion_icon).forEach((child)=> child.src=src_close);
+    accordion_icon_all.forEach((child)=> child.classList.remove(src_open));
+    accordion_icon_all.forEach((child)=> child.classList.add(src_close));
 })
